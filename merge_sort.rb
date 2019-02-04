75a289f325d3f386f6a5d671c707224cb0f26142
Rudi Boshoff <rudiboshoff15@gmail.com>
2019-02-04 15:55:19 +0200

    Docs: adds merge sort using recursion

---
+++ merge_sort.rb
@@ -0,0 +1,30 @@
+# Rudi Boshoff
+
+n = [4,8,6,2,1,7,5,3]
+
+def merge_sort(array)
+  if array.length < 2
+    return array
+  else
+    size = array.length
+    # break array into two parts
+    # sort left half
+    # sort right half
+    left = merge_sort(array[0..size/2-1])
+    right = merge_sort(array[size/2..-1])
+    # merge sorted halves
+    merge(left, right)
+  end
+end
+
+def merge(left, right)
+    merged_arr = []
+
+    until left.empty? || right.empty?
+        merged_arr << (left.first <= right.first ? left.shift : right.shift)
+    end
+
+    merged_arr + left + right
+end
+
+print merge_sort(n)
