ec5f557eaa00d38c22d11a12add9e46a3cddcc12
Rudi Boshoff <rudiboshoff15@gmail.com>
2019-02-04 14:32:12 +0200

    Features: adds looped fibonacci solution and recursive fibonacci solution

---
+++ fibonacci.rb
@@ -0,0 +1,22 @@
+# Rudi Boshoff
+
+def fibs_rec(n)
+  return 0 if n == 0
+  return 1 if n == 1
+  fibs_rec(n-1) + fibs_rec(n-2)
+end
+
+
+def fibs(n)
+  solutions = [0, 1]
+  while solutions.length <= n
+    solutions << solutions[-1] + solutions[-2]
+  end
+  solutions[n]
+end
+
+n = 10
+print "fibs_rec(#{n}) = "
+puts  fibs_rec(n)
+print "fibs(#{n}) = "
+puts fibs(n)
